75731dab751e91844d2ea090b787f1d8d6c2e997
nezi404 <nadapranadaaa@gmail.com>
2023-02-09 15:18:29 +0000

    nao perca

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const {MongoClient} = require("mongodb");
+const URL_DO_DB = "mongodb+srv://admin:C4dqiihlUXHYT7cc@cluster0.03neohq.mongodb.net";
+const NOME_DO_DB = "banco-de-dados-de-passaros";
+const collection = URL_DO_DB.collection
+
+async function main(){
+
+//conectar ao bd, o mongo
+console.log("Comecando a se conctar com o banco de dados");
+const client = await MongoClient.connect(URL_DO_DB);
+console.log("Conectado com o banco de dados");
+
 const app = express();
 
 // O que vier no body da requisição, está em JSON
@@ -21,7 +33,8 @@ const passaros = ["bem-te-vi", "sabiá", "cambacica"];
 // CRUD -> Lista de informações
 
 // Endpoint Read All -> [GET] /item
-app.get("/passaro", function (req, res) {
+app.get("/passaro", async function (req, res) {
+  const documentos = await collection.find().toArray();
   res.send(itens);
 });
 
@@ -40,4 +53,8 @@ app.post("/passaro", function (req, res) {
   res.send("Item criado com sucesso!");
 });
 
+
 app.listen(3000);
+}
+
+main();
