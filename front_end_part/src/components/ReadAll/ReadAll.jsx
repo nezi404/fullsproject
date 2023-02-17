6c34379320dc344084e7fbaf8a99810f5be13f06
nezi404 <nadapranadaaa@gmail.com>
2023-02-17 15:28:16 +0000

    mexendo com css, mas ainda implementanado o bd

--- front_end_part/src/components/ReadAll/ReadAll.jsx
+++ front_end_part/src/components/ReadAll/ReadAll.jsx
@@ -1,10 +1,30 @@
-import ReactDOM from "react-dom"
-function ReadAll()  {
-    return (
-    <div className="Card">
-            <img src="https://s3.amazonaws.com/media.wikiaves.com.br/images/6411/1146183_b5abd4a00de05616c813e78344d1db8e.jpg" />
-        </div>
-    )
+import "./ReadAll.css";
+import Card from "../Card/Card";
+
+const itens = [
+    {
+        _id: 1234,
+        nome : "pato",
+        imagemUrl: "https://www.petz.com.br/blog/wp-content/uploads/2022/06/como-cuidar-de-um-pato-2-1280x720.jpg"
+    },
+
+    {
+        _id: 1244,
+        nome : "arara canindé",
+        imagemUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Blue-and-Yellow-Macaw.jpg/280px-Blue-and-Yellow-Macaw.jpg"
+    }
+]
+
+const numero = 2;
+
+function ReadAll() {
+    return <div className="ReadAll">
+        {itens.map(function (item) {
+            console.log(item)
+            return <Card keys = { "card-" + item._id} item = {item}/>
+        })}
+       
+    </div>
 }
 
-export default ReadAll
+export default ReadAll;
